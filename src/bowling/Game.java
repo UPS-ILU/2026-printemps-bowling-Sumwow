74b8c832dfe159ef6e15dde14f91cd8a362cc7b9
Sumwow <matteolafitte31@gmail.com>
2026-04-22 14:43:10 +0200

    test partie réussi

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -2,12 +2,38 @@ package bowling;
 
 public class Game {
 	
-	int totalquilles = 0;
-	public void lancer(int nbrquilles) {
-		totalquilles+=nbrquilles;
-	}
+	private int[] partie = new int[21];
+	private int indice = 0;
 	
+	public void lancer(int nbrquilles) {
+		partie[indice]=nbrquilles;
+		indice++;
+	}
+
 	public int score() {
-		return totalquilles;
+		int total = 0;
+		int i = 0;
+
+		for (int frame = 0; frame < 10; frame++) {
+			
+			// STRIKE
+			if (partie[i] == 10) {
+				total += 10 + partie[i+1] + partie[i+2];
+				i += 1;
+			}
+			
+			// SPARE
+			else if (partie[i] + partie[i+1] == 10) {
+				total += 10 + partie[i+2];
+				i += 2;
+			}
+			
+			// NORMAL
+			else {
+				total += partie[i] + partie[i+1];
+				i += 2;
+			}
+		}
+		return total;
 	}
 }
--- test/bowling/GameTest.java
+++ test/bowling/GameTest.java
@@ -10,6 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 public class GameTest {
 	Game game;
 	
+	public void plusieurscoups(int nbrlancer, int nbrquilles) {
+		for (int i = 0; i< nbrlancer; i++) {
+			game.lancer(nbrquilles);
+		}
+	}
+	
 	@BeforeEach
 	public void setup() {
 		game = new Game();
@@ -23,8 +29,40 @@ public class GameTest {
 	
 	@Test
 	public void test2() {
-		game.lancer(10);
-		assertEquals(10,game.score());
+		game.lancer(5);
+		assertEquals(5,game.score());
 	}
 	
+	@Test
+	public void test3() {
+		plusieurscoups(4,3);
+		assertEquals(12,game.score());
+	}
+	
+	@Test
+	public void strike() {
+		game.lancer(10);
+		game.lancer(5);
+		game.lancer(3);
+		assertEquals(26,game.score());
+	}
+	
+	@Test
+	public void spare() {
+		game.lancer(4);
+		game.lancer(6);
+		game.lancer(3);
+		assertEquals(16,game.score());
+	}
+	
+	@Test
+	public void partie() {
+		game.lancer(4);
+		game.lancer(6);
+		game.lancer(3);
+		game.lancer(3);
+		game.lancer(10);
+		plusieurscoups(15,4);
+		assertEquals(93, game.score());
+	}
 }
