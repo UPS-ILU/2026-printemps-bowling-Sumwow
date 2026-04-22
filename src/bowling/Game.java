b64ff9f9a6f216e2ffcc5ab0aa3be3ee0c722c99
Sumwow <matteolafitte31@gmail.com>
2026-04-22 14:06:55 +0200

    test 1 passé

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -1,7 +1,13 @@
 package bowling;
 
 public class Game {
+	
+	int totalquilles = 0;
+	public void lancer(int nbrquilles) {
+		totalquilles+=nbrquilles;
+	}
+	
 	public int score() {
-		return 0;
+		return totalquilles;
 	}
 }
--- test/bowling/GameTest.java
+++ test/bowling/GameTest.java
@@ -4,15 +4,27 @@ package bowling;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class GameTest {
 	Game game;
 	
-	@Test
-	public void Test1() {
+	@BeforeEach
+	public void setup() {
 		game = new Game();
+	}
+	
+	@Test
+	public void test1() {
+		game.lancer(1);
 		assertEquals(1,game.score());
+	}
+	
+	@Test
+	public void test2() {
+		game.lancer(10);
+		assertEquals(10,game.score());
 	}
 	
 }
